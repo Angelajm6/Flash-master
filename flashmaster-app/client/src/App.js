533f2cdd7e56344cf2269062a66f16cde0742a98
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-04-20 19:53:40 -0400

    added routes, httplink, and authLink

--- flashmaster-app/client/src/App.js
+++ flashmaster-app/client/src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { Browser as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Teacher from './pages/Teacher';
+import Student from './pages/Student';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />}
+              />
+              <Route 
+                path="/login" 
+                element={<Login />}
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />}
+              />
+              <Route 
+                path="/me" 
+                element={<Teacher />}
+              />
+              <Route 
+                path="/me" 
+                element={<Student />}
+                />
+              <Route 
+                path="/teachers/:teacherId"
+                element={<Teacher />}
+              />
+              <Route
+              path="/students/:studentId"
+              element={<Student />}
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
