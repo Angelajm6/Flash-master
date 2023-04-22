c9c516fd130569d1392d648034566c343765953f
ima2132 <ima2132@columbia.edu>
2023-04-22 19:52:58 -0400

    entered teachers role info for data.js file

--- flashmaster-app/server/utils/data.js
+++ flashmaster-app/server/utils/data.js
@@ -6,8 +6,24 @@ const ROLE = {
   module.exports = {
     ROLE: ROLE,
     users: [
-      { id: 1, name: 'Kyle', role: ROLE.ADMIN },
-      { id: 2, name: 'Sally', role: ROLE.BASIC },
+      { id: 101, name: "Alberto Harrison", role: ROLE.ADMIN },
+      { id: 102, name: "Abigail Carter", role: ROLE.ADMIN },
+      { id: 103, name: "Allen Barber", role: ROLE.ADMIN },
+      { id: 104, name: "Brenda Wang", role: ROLE.ADMIN },
+      { id: 105, name: "Betty Hansen", role: ROLE.ADMIN },
+      { id: 106, name: "Cathy Johnson", role: ROLE.ADMIN },
+      { id: 107, name: "Cindy Rogers", role: ROLE.ADMIN },
+      { id: 108, name: "Jack Webster", role: ROLE.ADMIN },
+      { id: 109, name: "Rob Williams", role: ROLE.ADMIN },
+      { id: 110, name: "Jenny Swift", role: ROLE.ADMIN },
+      { id: 3, name: 'Joe', role: ROLE.BASIC }
+      { id: 3, name: 'Joe', role: ROLE.BASIC }
+      { id: 3, name: 'Joe', role: ROLE.BASIC }
+      { id: 3, name: 'Joe', role: ROLE.BASIC }
+      { id: 3, name: 'Joe', role: ROLE.BASIC }
+      { id: 3, name: 'Joe', role: ROLE.BASIC }
+      { id: 3, name: 'Joe', role: ROLE.BASIC }
+      { id: 3, name: 'Joe', role: ROLE.BASIC }
       { id: 3, name: 'Joe', role: ROLE.BASIC }
     ],
     projects: [
