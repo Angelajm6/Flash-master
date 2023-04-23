8356dce3b53f3cc2ca51fe1dae53f9d07b5f4a1e
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-04-22 20:27:02 -0400

    made changes in the seearchbar component

--- flashmaster-app/client/src/components/SearchComponent/index.js
+++ flashmaster-app/client/src/components/SearchComponent/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { useQuery } from '../../utils/queries';
 
-import { GET_USERS, GET_PACKS } from '../../utils/queries';
+import { QUERY_SINGLE_USER, QUERY_ME } from '../../utils/queries';
 
 function SearchBar() {
     const [searchTerm, setSearchTerm] = useState('');
-    const [searchUser, {data: GET_USERS}] = useQuery(GET_USERS);
-    const [searchPack, {data: GET_PACKS}] = useQuery(GET_PACKS);
+    const [searchUser, {data: QUERY_SINGLE_USER}] = useQuery(QUERY_SINGLE_USER);
+    const [searchMe, {data: QUERY_ME}] = useQuery(QUERY_ME);
 
     const handleSearch = (event) => {
         event.preventDefault();
         searchUser({ variables: {name: searchTerm}});
-        searchPack({variables: {name: searchPack}});
+        searchMe({variables: {name: searchPack}});
     };
 
 }
