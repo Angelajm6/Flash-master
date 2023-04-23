38e6b480999189170cfd58fc0dd22e2f35a8a8ef
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-04-22 20:55:36 -0400

    made changes to search bar component

--- flashmaster-app/client/src/components/SearchComponent/index.js
+++ flashmaster-app/client/src/components/SearchComponent/index.js
@@ -1,19 +1,45 @@
 import React from 'react';
-import { useQuery } from '../../utils/queries';
+import { useQuery } from '@apollo/client';
 
 import { QUERY_SINGLE_USER, QUERY_ME } from '../../utils/queries';
 
 function SearchBar() {
     const [searchTerm, setSearchTerm] = useState('');
-    const [searchUser, {data: QUERY_SINGLE_USER}] = useQuery(QUERY_SINGLE_USER);
-    const [searchMe, {data: QUERY_ME}] = useQuery(QUERY_ME);
+    const [searchUser, { data: usersData }] = useQuery(QUERY_SINGLE_USER);
+    const [searchMe, { data: myProfileData }] = useQuery(QUERY_ME);
 
     const handleSearch = (event) => {
         event.preventDefault();
-        searchUser({ variables: {name: searchTerm}});
-        searchMe({variables: {name: searchPack}});
+        searchUser({ variables: { name: searchTerm } });
+        searchMe({ variables: { name: searchMe } });
     };
+    return (
+        <div className="search">
+            <div className="searchInputs">
+                <form onSubmit={handleSearch}>
+                    <input type="text" placeholder='Search!' value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)}>
+                        <button type="submit">Search</button>
+                        {usersData && (
+                            <ul>
+                                {usersData.users.map((user) => {
+                                    <li key={user._id}>{user.name}</li>
+                                })}
+                            </ul>
+                        )}
+                        {myProfileData && (
+                            <ul>
+                                {myProfileData.users.map((user) => (
+                                    <li key={user._id}>{user.name}</li>
+                                ))}
+                            </ul>
+                        )}
+                    </input>
+                </form>
+            </div>
+        </div>
+    )
+};
 
-}
+export default SearchBar;
 
 
