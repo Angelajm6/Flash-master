15e797c558b840d6b276875a48a7a4243baa6e52
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-04-24 19:45:14 -0400

    made changes in searchcomponent to include flashdecks

--- flashmaster-app/client/src/components/SearchComponent/index.js
+++ flashmaster-app/client/src/components/SearchComponent/index.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 
-import { QUERY_SINGLE_USER, QUERY_ME } from '../../utils/queries';
+import { QUERY_SINGLE_USER, QUERY_FLASHDECK } from '../../utils/queries';
 
 function SearchBar() {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchUser, { data: usersData }] = useQuery(QUERY_SINGLE_USER);
-    const [searchMe, { data: myProfileData }] = useQuery(QUERY_ME);
+    const [searchTopic, { data: flashdeckData }] = useQuery(QUERY_FLASHDECK);
 
     const handleSearch = (event) => {
         event.preventDefault();
         searchUser({ variables: { name: searchTerm } });
-        searchMe({ variables: { name: searchMe } });
+        searchTopic({ variables: { name: searchTerm } });
     };
     return (
         <div className="search">
@@ -28,8 +28,8 @@ function SearchBar() {
                         )}
                         {myProfileData && (
                             <ul>
-                                {myProfileData.users.map((user) => (
-                                    <li key={user._id}>{user.name}</li>
+                                {flashdeckData.users.map((user) => (
+                                    <li key={flashdeck._id}>{flashdeck.topic}{flashdeck.author}{flashdeck.comments}</li>
                                 ))}
                             </ul>
                         )}
