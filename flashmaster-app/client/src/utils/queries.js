600e1d10297391b7287ac2a7ef21fadeca78b30f
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-04-24 19:16:36 -0400

    fixed conflicts

--- flashmaster-app/client/src/utils/queries.js
+++ flashmaster-app/client/src/utils/queries.js
@@ -1,6 +1,5 @@
 import { gql } from '@apollo/client';
 
-<<<<<<< HEAD
 export const QUERY_USERS =  gql`
 query allUsers {
   users {
@@ -28,90 +27,7 @@ export const QUERY_ME = gql`
       _id
       name
       flashDecks
-=======
-export const QUERY_USERS = gql`
-  query getUsers {
-    users {
-        name
-        _id
-        subject
-        email
-        role
-        flashdecks
     }
   }
 `;
 
-export const QUERY_SINGLE_USER = gql`
-  query getUser {
-    user {
-        name
-        _id
-        subject
-        email
-        role
-        flashdecks
-    }
-  }
-`;
-
-export const QUERY_COMMENTS = gql`
-  query getComments {
-    comments {
-        _id
-        commentText
-        commentAuthor
-        createdAt
-    }
-  }
-`;
-
-export const QUERY_SINGLE_COMMENT = gql`
-  query getComment {
-    comment {
-        _id
-        commentText
-        commentAuthor
-        createdAt
-    }
-  }
-`;
-
-export const QUERY_FLASHDECKS = gql`
-  query getFlashdecks($topic: String!) {
-    flashdecks(topic: $topic) {
-        _id
-        topic
-        author
-        createdAt
-        comments
-    }
-  }
-`;
-
-export const QUERY_SINGLE_FLASHDECK = gql`
-  query getFlashdeck($topic: String!) {
-    flashdeck(topic: $topic) {
-        _id
-        topic
-        author
-        createdAt
-        comments
->>>>>>> c563bd446386bedeaa639f65a853382f01362305
-    }
-  }
-`;
-
-<<<<<<< HEAD
-=======
-// export const QUERY_POST = gql`
-//   query getPost($id: Int!) {
-//     post(id: $id) {
-//       id
-//       authorId
-//       title
-//       votes
-//     }
-//   }
-// `;
->>>>>>> c563bd446386bedeaa639f65a853382f01362305
