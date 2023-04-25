652e15107a70359a17be0abc1b315c8a9653128c
Matthew M. Provost <provostmat@gmail.com>
2023-04-25 10:43:44 -0400

    added basic flashcard functionality and styling

---
+++ flashmaster-app/client/src/components/FlashCard/FlashCard.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './FlashCard.css';
+
+export default function FlashCard({ flashcard }) {
+    const [flip, setFlip] = useState(false)
+
+
+  return (
+    <div 
+        className={ `card ${flip ? 'flip' : ''}`}
+        onClick={() => setFlip(!flip)}
+    >
+        <div className="front">
+            {flashcard.question}
+            <div className="flashcard-options">
+                {flashcard.options.map(option => {
+                    return <div className="flashcard-option">{option}</div>
+                })}
+            </div>
+        </div>
+        <div className="back">{flashcard.answer}</div>
+        {flip ? flashcard.answer : flashcard.question}
+
+      
+    </div>
+  )
+}
