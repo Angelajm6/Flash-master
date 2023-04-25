4cf3eb1cf258821376b4944c29f155f57ca40ea4
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-04-24 20:18:21 -0400

    Word api/feature (#15)
    * added API.js file for fetch request
    
    * added axios dependency
    
    * added dotenv dependency in client package.json
    
    * added api end point and .env file for api key
    
    * added to api.js and added key to .env file
    
    * fixed .env file for react app
    
    * added files for search bar
    
    * added search bar component functionality
    
    * added queries
    
    * made changes in the seearchbar component
    
    * made changes to search bar component
    
    * added navbar component
    
    * added header
    
    * added results page
    
    * added changes
    
    * fixed conflicts
    
    * pulled
    
    * fixed errors with signup, login, mutations, and app.js
    
    * added flashdeck query in client/utils/queries folder
    
    * made changes in searchcomponent to include flashdecks
    
    * made changes to the search component
    
    * made changes
    
    * deleted a line

---
+++ flashmaster-app/client/src/pages/Home.js
@@ -0,0 +1,11 @@
+//import React from 'react';
+//import Searchbar from '../components/SearchComponent';
+//import { QUERY_USERS } from '../../utils/queries';
+
+//function Homepage() {
+    //return(
+        //<div className="Homepage">
+            //<Searchbar placeholder="Enter a topic or username..." data={ QUERY_USERS } />
+        //</div>
+   // )
+//}
---
+++ flashmaster-app/client/src/pages/Results.js
@@ -0,0 +1,19 @@
+//import React from 'react';
+//import Link from '@apollo/client'
+
+
+//function ResultList(props) {
+    //return (
+       // <ul className="list-group">
+           // {props.usersData.map((result) => {
+              // <li className="list-group-item" key={user.id}>
+                   // <Link className="btn btn-lg btn-primary m-2" to="/teachers/:teacherId">
+                   // {user.name}
+              //</Link>
+              //  </li>
+            //})}
+       // </ul>
+   // )
+//};
+
+//export default ResultList;
