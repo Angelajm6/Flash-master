8b3ec5376f407b2614c711e93066b6ae93c9bc72
angelajm6 <angelajaume.m@gmail.com>
2023-04-25 18:48:53 -0400

    finished Donation page

--- flashmaster-app/client/src/pages/Teacher/Donation.js
+++ flashmaster-app/client/src/pages/Teacher/Donation.js
@@ -10,11 +10,32 @@ import Auth from '../utils/auth';
 function Teacher(props) {
   const [donationAmount, setDonationAmount] = useState(0);
 
-  // HandleDonationChange function updates the donationAmount state when the user enters a donation amount
+
+// HandleDonationChange function updates the donationAmount state when the user enters a donation amount
   const handleDonationChange = (event) => {
     setDonationAmount(event.target.value);
   }
 
+  // HandleDonationSubmit function is called When the user submits the form
+  const handleDonationSubmit = (event) => {
+    event.preventDefault();
+    // Replace this with your actual donation logic
+    alert(`Thank you for donating $${donationAmount} to ${props.name}!`);
+  }
+
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+      <form onSubmit={handleDonationSubmit}>
+        <label>
+          Donation amount:
+          <input type="number" value={donationAmount} onChange={handleDonationChange} />
+        </label>
+        <button type="submit">Donate</button>
+      </form>
+    </div>
+  );
 }
 
 export default Teacher;
