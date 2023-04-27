cd2d48e88e11a81b70072bb9096453ca846b90ed
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-04-26 21:44:30 -0400

    fixed merge conflicts

--- flashmaster-app/client/src/pages/Student/Student.js
+++ flashmaster-app/client/src/pages/Student/Student.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { gql, useQuery } from '@apollo/client';
-import Header from '../../components/Header/index';
-import Gallery from '../Teacher/Gallery';
-import Donation from '../Teacher/Donation';
-
-import { QUERY_USERS } from '../../utils/queries';
-import CommentList from '../../components/commentList/CommentList';
-import CommentForm from '../../components/CommentForm/index';
-import Login from '../Login';
-
-
-// Temporary name, intended to pull all flashDecks created by the user
-import { QUERY_FLASHDECKS_BY_USER } from '../../utils/queries'; 
-import { QUERY_USER } from '../../utils/queries';
-
-
-// If user is admin, props passed from home page to Teacher Portal page.
-const StudentPortal = ({ match }: RouteComponentProps<MatchParams>) =>{
-    //const { data, loading, error } =useQuery(User, {
-        //variables: {
-          //  id: match.params.id
-       // }
-   // });
-
-    return (
-    <>
-        <Header></Header>
-            {/*<h2>Welcome, {user.name}</h2>*/}
-            {/*<card className="Gallery"><Collections></Collections>*/}
-                <ul className="list-group">
-                {/*{props.results.map((result) => (*/}
-                    {/*<li className="list-group-item" key={result.flashDecks}></li>*/}
-               {/*} ))}*/}
-                </ul>
-            {/*</card>*/}
-=======
 // import React from 'react';
 // import Header from '../../components/Header/index';
 // import Gallery from '';
@@ -71,7 +33,6 @@ const StudentPortal = ({ match }: RouteComponentProps<MatchParams>) =>{
 //                 ))}
 //                 </ul>
 //             </card>
->>>>>>> main
         
 
             
