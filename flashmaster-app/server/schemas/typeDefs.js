7c20c483c386c65758a1240e6272d06eaeb9dfaf
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-04-27 22:03:11 -0400

    merge with main

--- flashmaster-app/server/schemas/typeDefs.js
+++ flashmaster-app/server/schemas/typeDefs.js
@@ -27,6 +27,17 @@ const typeDefs = gql`
     comments: [String]!
   }
 
+  type Donation {
+    _id: ID
+    teacher: String
+    amount: Int
+  }
+
+type Checkout {
+  _id: ID
+  donation_amount: Int
+}  
+
   type Auth {
     token: ID!
     user: User
@@ -36,20 +47,21 @@ const typeDefs = gql`
     users: [User]
     user(name: String!): User
     me(name: String!): User
-    flashcards(topic: String!, userId: ID): Flashcards
+    flashcards(topic: String!, userId: ID): Flash
     flash(userId: ID): Flash
     comments(name: String): [Comment]
     comment(commentId: ID!): Comment
-    donation(userId: ID, teacher: String!, amount: INT): Donation
+    donation(userId: ID, teacher: String!, amount: Int): Donation
+    checkout(userId: ID, donation_amount: Int): Checkout
   }
 
-  type Mutation {
+  type Mutation { 
     addUser(name: String!, email: String!, password: String!): Auth
     updateUser(name: String, subject: String, _id: ID, email: String, password: String, role: String, Flash: [String]!): User
     login(email: String!, password: String!): Auth
     addComment(commentId: ID!, commentText: String!, commentAuthor: String!): Comment
     addFlashcard(flashName: String!, flashAuthor: String!, createdAt: String): Flash
-    addDonation(userId: ID, teacher: String!, amount: INT)  
+    addDonation(userId: ID, teacher: String!, amount: Int): Donation 
   }
 `;
 
