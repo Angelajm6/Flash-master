084f85d5de01a1cb366ef987b81b6a21eba5f8dd
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-04-27 20:31:49 -0400

    fixing mutations syntax, and adding queries

--- flashmaster-app/client/src/utils/queries.js
+++ flashmaster-app/client/src/utils/queries.js
@@ -7,13 +7,13 @@ query allUsers {
     _id
     name
     subject
-    flashDecks
+    Flash
   }
 }
 `;
-export const QUERY_FLASHDECK =  gql`
-query allUsers {
-  Flashdeck {
+export const QUERY_FLASHCARDS =  gql`
+query allFlash {
+  Flash {
     _id
     topic
     author
@@ -29,7 +29,7 @@ query singleUser($userId: ID!)  {
   user(userId: $userId) {
     _id
     name
-    flashDecks {
+    Flash {
       _id
       topic
       author
@@ -50,4 +50,61 @@ export const QUERY_ME = gql`
   }
 `;
 
+export const QUERY_COMMENTS =  gql`
+query allComments {
+  Comments {
+    _id
+    Flash
+    author
+    createdAt
+    comments
+  }
+}
+`;
 
+export const QUERY_SINGLE_COMMENT =  gql`
+query singleComment {
+  Comment {
+    _id
+    Flash
+    author
+    createdAt
+    comments
+  }
+}
+`;
+
+export const QUERY_DONATIONS = gql`
+  query  allDonations{
+    Donations {
+      _id
+      teacher
+      amount
+    }
+  }
+`;
+
+export const QUERY_SINGLE_DONATION = gql`
+  query  singleDonation($name: String!){
+    Donation(name: $name) {
+      _id
+      teacher
+      amount
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query  singleCheckout($name: String!, $email: String!){
+    Checkout(name: $name, email: $email) {
+      _id
+      name
+      email
+      Donation{
+        _id
+        teacher
+        amount
+      }
+    }
+  }
+`;
