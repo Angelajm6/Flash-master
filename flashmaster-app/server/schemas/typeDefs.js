08feb353b87fcc8ff485c281a4f3136a161794e3
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-04-27 20:13:24 -0400

    Feature/type defs (#35)
    * review changes
    
    * main branch commit of resolvers update and typeDef update
    
    * review and update after wed
    
    * made syntax changes to mutations
    
    * added more queries and mutations
    
    * added more to donation

--- flashmaster-app/server/schemas/typeDefs.js
+++ flashmaster-app/server/schemas/typeDefs.js
@@ -35,9 +35,12 @@ const typeDefs = gql`
   type Query {
     users: [User]
     user(name: String!): User
+    me(name: String!): User
+    flashcards(topic: String!, userId: ID): Flashcards
+    flash(userId: ID): Flash
     comments(name: String): [Comment]
     comment(commentId: ID!): Comment
-
+    donation(userId: ID, teacher: String!, amount: INT): Donation
   }
 
   type Mutation {
@@ -45,7 +48,8 @@ const typeDefs = gql`
     updateUser(name: String, subject: String, _id: ID, email: String, password: String, role: String, Flash: [String]!): User
     login(email: String!, password: String!): Auth
     addComment(commentId: ID!, commentText: String!, commentAuthor: String!): Comment
-    addFlashcard(flashName: String!, flashAuthor: String!, createdAt: String): Flash  
+    addFlashcard(flashName: String!, flashAuthor: String!, createdAt: String): Flash
+    addDonation(userId: ID, teacher: String!, amount: INT)  
   }
 `;
 
