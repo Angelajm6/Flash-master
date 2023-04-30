b963fef0e675f9e534a24220ef998499c8293a09
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-04-29 21:29:57 -0400

    adding chakra themes and components

---
+++ flashmaster-app/client/src/assets/style.js
@@ -0,0 +1,20 @@
+import { Box, Text } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import { chakra } from '@chakra-ui/react';
+import Textarea from 'react-input-autoresize';
+
+const AutoResizeInput = chakra(Textarea)
+
+function Example() {
+  return <AutoResizeInput bg='red.200' fontSize='12px' />
+};
+Example();
+
+const config = {
+    initialColorMode: 'light',
+    useSystemColorMode: false,
+  }
+
+  const theme = extendTheme({ config })
+
+export default theme
