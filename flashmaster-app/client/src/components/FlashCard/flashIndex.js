6cc8cae9f963116d8d1f765c3d1e181378b706ca
Matthew M. Provost <provostmat@gmail.com>
2023-05-01 17:46:59 -0400

    added delete function and basic questions/answers for flashcards

--- flashmaster-app/client/src/components/FlashCard/flashIndex.js
+++ flashmaster-app/client/src/components/FlashCard/flashIndex.js
@@ -22,6 +22,12 @@ export default function FlashIndex() {
     answerEl.current.value = '';
   }
 
+  function handleDelete(index) {
+    const newFlashcards = [...flashcards];
+    newFlashcards.splice(index, 1);
+    setFlashcards(newFlashcards);
+  }
+
   return (
     <>
       <form className="createCard" onSubmit={handleSubmit}>
@@ -36,24 +42,47 @@ export default function FlashIndex() {
         </div>
       </form>
       <div className="container">
-        <FlashCardList flashcards={flashcards}/>
+        <FlashCardList flashcards={flashcards} handleDelete={handleDelete} />
       </div>
     </>
   );
 }
 
-
 const SAMPLE_FLASHCARDS = [
   {
-    question: "What's Q1?",
-    answer: 'A1'
+    question: "In To Kill a Mockingbird, What was Scout's first 'crime' at school?",
+    answer: "Scout's first crime was that she could already read."
   },
   {
-    question: 'Question 2?',
-    answer: 'Answer 2'
+    question: "In To Kill A Mockingbird, What did Scout and Jem find in the Radleys' tree?",
+    answer: "They found gum and two Indian head pennies."
   },
   {
-    question: 'Q3?',
-    answer: 'Answer 3'
-  }
+    question: 'What was the Boo Radley game?',
+    answer: 'Jem, Dill and Scout reenacted the few facts and many peculiar stories they had heard about the Radleys.'
+  },
+  {
+    question: "Identify Miss Maudie.",
+    answer: "Miss Maudie is another neighbor, about the age of Atticus. She is open-minded and enjoys the children's company."
+  },
+  {
+    question: "What else did Jem and Scout find in the Radleys' tree?",
+    answer: "They found a ball of twine, two figures (resembling themselves) carved from soap, and a broken watch."
+  },
+  {
+    question: "Why would there be no more surprises in the tree?",
+    answer: "Mr. Nathan Radley cemented the hole closed."
+  },
+  {
+    question: " Identify Cecil Jacobs",
+    answer: "Cecil Jacobs was a boy at Scout's school who first made her aware that Atticus was defending a black man."
+  },
+  {
+    question: "What did Mr. Heck Tate's mob want?",
+    answer: "They wanted to make sure Atticus and Tom Robinson would be all right."
+  },
+  {
+    question: "What was the purpose of Walter Cunningham's mob?",
+    answer: "Cunningham's mob wanted to get to Tom Robinson to inflict their own justice upon him. If that meant they had to beat up Atticus, they were willing to do that"
+  },
 ];
