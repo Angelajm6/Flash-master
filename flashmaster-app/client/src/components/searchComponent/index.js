f020cd0e7bf7215a9c86a68bf63f66b1d723a9dd
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-01 18:56:35 -0400

    hopefully this is the styling

--- flashmaster-app/client/src/components/searchComponent/index.js
+++ flashmaster-app/client/src/components/searchComponent/index.js
@@ -1,58 +1,52 @@
 import React, { useState } from 'react';
 import { useLazyQuery } from '@apollo/client';
-import { QUERY_SINGLE_USER } from '../../utils/queries';
 import { Link } from 'react-router-dom';
-
-const Searchbar = () => {
-    const [search, setSearch] = useState('');
-    const [searchedResults, setSearchedResults] = useState([]);
-    const [searchSingleUser, { loading, data }] = useLazyQuery(QUERY_SINGLE_USER);
-
-    const handleSearch = (e) => {
-        e.preventDefault();
-        searchSingleUser({
-            variables: { userID: search },
-            onCompleted: (data) => {
-                setSearchedResults(data.users);
-            }
-        });
-    };
-
-    return (
-        <div>
-            <form onSubmit={handleSearch}>
-                <input
-                    type="text"
-                    placeholder="Search For A User"
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                />
-                <button type="submit">Search</button>
-            </form>
-            {loading ? (
-                <div>Loading...</div>
-            ) : (
-                <ul>
-                    {searchedResults.length > 0 ? (
-                        <ul>
-                            {searchedResults.map((result) => (
-                                <li key={result._id}>
-                                    <Link to={`/users/${result._id}`}>
-                                        {result.username}
-                                    </Link> 
-                                </li>
-                            ))}
-                        </ul>
-                    ) : search && (
-                        <div>No results found.</div>
-                    )}
-                </ul>
-            )}
-            {!search && (
-                <div>Please Enter A User Name</div>
-            )}
+import { QUERY_SINGLE_USER } from '../../utils/queries';
+import auth from '../../utils/auth';
+const SearchBar = () => {
+    const [searchName, setSearchName] = useState('');
+    const [searchUser, { loading, data, error }] = useLazyQuery(QUERY_SINGLE_USER, {
+      context: {
+        headers: {
+          Authorization: `Bearer ${auth.getToken()}`,
+        },
+      },
+    });
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (searchName.trim()) {
+      searchUser({ variables: { name: searchName } });
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search for a user by name..."
+          value={searchName}
+          onChange={(e) => setSearchName(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error.message}</p>}
+      {data && data.user ? (
+        <div key={data.user._id}>
+          <p>Name: {data.user.name}</p>
+          <p>Subject: {data.user.subject}</p>
+          <p>Role: {data.user.role}</p>
+          <Link to={`/users/${data.user._id}`}>Go to profile</Link>
         </div>
-    );
+      ) : (
+        <p>No user found</p>
+      )}
+    </div>
+  );
 };
+export default SearchBar;
 
-export default Searchbar;
+
+
+
+
