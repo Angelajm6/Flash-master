af92194f2312b5b8ab3affd249d935c0c724c1e9
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-04-30 21:07:47 -0400

    created a flashdeck.json seed file

--- flashmaster-app/server/seeders/seed.js
+++ flashmaster-app/server/seeders/seed.js
@@ -1,11 +1,20 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Comment, Flash } = require('../models');
 const userSeeds = require('./userSeeds.json');
+const flashSeeds = require('./flashdeck.json');
+const commentSeeds = require('./commentSeeds.json');
 
 db.once('open', async () => {
   try {
+    // clean database
     await User.deleteMany({});
-    await User.create(userSeeds);
+    await Comment.deleteMany({});
+    await Flash.deleteMany({});
+
+    // bulk create each model
+    await User.insertMany(userSeeds);
+    await Flash.insertMany(flashSeeds);
+    await Comment.insertMany(commentSeeds);
 
     console.log('all done!');
     process.exit(0);
