9515d83f465a5fddc201d73226162108b61f9b7c
Matthew M. Provost <provostmat@gmail.com>
2023-04-30 22:10:44 -0400

    card seed data and flashcards with basic functionality added

--- flashmaster-app/client/src/components/FlashCard/index.js
+++ flashmaster-app/client/src/components/FlashCard/index.js
@@ -1,95 +1,95 @@
-import React, { useState, useRef, useEffect } from 'react';
-import FlashCardList from './FlashCardList';
-import { QUERY_FLASHCARDS } from '../../utils/queries';
-import { UPDATE_FLASHCARD } from '../../utils/mutations';
-import { useQuery, useMutation } from '@apollo/client';
-// import './FlashCard/FlashCard.css';
+// import React, { useState, useRef, useEffect } from 'react';
+// import FlashCardList from './FlashCardList';
+// import { QUERY_FLASHCARDS } from '../../utils/queries';
+// import { UPDATE_FLASHCARD } from '../../utils/mutations';
+// import { useQuery, useMutation } from '@apollo/client';
+// // import './FlashCard/FlashCard.css';
 
-// import { QUERY_SINGLE_USER } from './utils/queries';
-
-
-// Pass users array to the List component as a prop
-export default function Flash() {
-  // const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
-  // Remove (SAMPLE_FLASHCARDS) and replace with empty array
-  const [flashcards, setFlashcards] = useState(null);
-  const [question, setQuestion] = useState('');
-  const [answer, setAnswer] = useState('');
-
-  const { loading, error, data: queryData } = useQuery(QUERY_FLASHCARDS);
-
-  useEffect(() => {
-    if (queryData) {
-      setFlashcards(queryData.cards);
-    }
-  }, [queryData]);
-
-  const [addCard, { data }] = useMutation(UPDATE_FLASHCARD);
+// // import { QUERY_SINGLE_USER } from './utils/queries';
 
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addCard({variables: { question: question, answer: answer}});
-    setQuestion('');
-    setAnswer('');
-    setFlashcards({question: setQuestion, answer: setAnswer});
+// // Pass users array to the List component as a prop
+// export default function Flash() {
+//   // const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+//   // Remove (SAMPLE_FLASHCARDS) and replace with empty array
+//   const [flashcards, setFlashcards] = useState(null);
+//   const [question, setQuestion] = useState('');
+//   const [answer, setAnswer] = useState('');
+
+//   const { loading, error, data: queryData } = useQuery(QUERY_FLASHCARDS);
+
+//   useEffect(() => {
+//     if (queryData) {
+//       setFlashcards(queryData.cards);
+//     }
+//   }, [queryData]);
+
+//   const [addCard, { data }] = useMutation(UPDATE_FLASHCARD);
+
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     addCard({variables: { question: question, answer: answer}});
+//     setQuestion('');
+//     setAnswer('');
+//     setFlashcards({question: setQuestion, answer: setAnswer});
     
-  };
+//   };
  
-  const questionEl = useRef()
-  const answerEl = useRef()
+//   const questionEl = useRef()
+//   const answerEl = useRef()
 
  
     
-  return (
-    <>
+//   return (
+//     <>
 
-      <form className="createCard" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="question">Question</label>
-            <input type="text" 
-            value={question}
-            id="question" ref={questionEl}
-            onChange={(e) => setQuestion(e.target.value)} >
-            </input>
-          <label htmlFor="answer">Answer</label>
-          <input type="text"
-          value={answer}
-          id="answer" ref={answerEl}>
-            onChange={(e) => setAnswer(e.target.value)} 
-          </input>
-        </div>
-        <div className="form-group">
-          <button className="btn">Create</button>
-        </div>
-      </form>
-      <div className="container">
-        {loading && <p>Loading...</p>}
-        {error && <p>Error :(</p>}
-        {data && data.Flash.flashCard.map(flashCard => (
-          <FlashCardList key={Flash.id} flashCard={flashCard} />
-          ))} 
-          <FlashCardList flashcards={flashcards}/>
+//       <form className="createCard" onSubmit={handleSubmit}>
+//         <div className="form-group">
+//           <label htmlFor="question">Question</label>
+//             <input type="text" 
+//             value={question}
+//             id="question" ref={questionEl}
+//             onChange={(e) => setQuestion(e.target.value)} >
+//             </input>
+//           <label htmlFor="answer">Answer</label>
+//           <input type="text"
+//           value={answer}
+//           id="answer" ref={answerEl}>
+//             onChange={(e) => setAnswer(e.target.value)} 
+//           </input>
+//         </div>
+//         <div className="form-group">
+//           <button className="btn">Create</button>
+//         </div>
+//       </form>
+//       <div className="container">
+//         {loading && <p>Loading...</p>}
+//         {error && <p>Error :(</p>}
+//         {data && data.Flash.flashCard.map(flashCard => (
+//           <FlashCardList key={Flash.id} flashCard={flashCard} />
+//           ))} 
+//           <FlashCardList flashcards={flashcards}/>
         
-      </div>
-    </>
-  );
-}
+//       </div>
+//     </>
+//   );
+// }
 
-// const SAMPLE_FLASHCARDS = [
-//   {
-//     id: 1,
-//     question: "What's Q1?",
-//     answer: 'A1'
-//   },
-//   {
-//     id: 2,
-//     question: 'Question 2?',
-//     answer: 'Answer 2'
-//   },
-//   {
-//     id: 3,
-//     question: 'Q3?',
-//     answer: 'Answer 3'
-//   }
-// ];
+// // const SAMPLE_FLASHCARDS = [
+// //   {
+// //     id: 1,
+// //     question: "What's Q1?",
+// //     answer: 'A1'
+// //   },
+// //   {
+// //     id: 2,
+// //     question: 'Question 2?',
+// //     answer: 'Answer 2'
+// //   },
+// //   {
+// //     id: 3,
+// //     question: 'Q3?',
+// //     answer: 'Answer 3'
+// //   }
+// // ];
