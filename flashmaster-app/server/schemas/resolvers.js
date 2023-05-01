9fb069ce3084c9ee70e0416b90ab035644d1dfb7
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-04-30 21:15:58 -0400

    Graphql/feature (#53)
    * git pull sun 1:00
    
    * package.json updates-taking out 'next' dependency
    
    * created a flashdeck.json seed file

--- flashmaster-app/server/schemas/resolvers.js
+++ flashmaster-app/server/schemas/resolvers.js
@@ -2,6 +2,26 @@ const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/jwtAuth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 const { User, Flash, Comment } = require('../models');
+const { GraphQLScalarType } = require('graphql');
+
+const DateType = new GraphQLScalarType({
+  name: 'Date',
+  description: 'Date custom scalar type',
+  parseValue(value) {
+    return new Date(value); // value from the client
+  },
+  serialize(value) {
+    return value.getTime(); // value sent to the client
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(ast.value) // ast value is always in string format
+    }
+    return null;
+  },
+});
+DateType();
+
 
 const resolvers = {
     Query: {
