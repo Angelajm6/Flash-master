9bc810cdaa988ee1e77536b7b6949c9b26c9dae5
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-01 18:45:17 -0400

    added style to homepage

--- flashmaster-app/client/src/pages/Home.js
+++ flashmaster-app/client/src/pages/Home.js
@@ -1,7 +1,19 @@
 import React, { useState} from 'react';
-import Header from '../components/Header/index'
+import { Button } from 'antd';
+import Header from '../components/Header/index';
+import {  Breadcrumb, Layout, Menu, theme  } from 'antd';
+import {
+    UserOutlined,
+    VideoCameraOutlined,
+  } from '@ant-design/icons';
+
+const { Content, Footer } = Layout;
 
 function Home() {
+    const {
+        token: { colorBgContainer },
+      } = theme.useToken();
+
     const [wordOfTheDay, setWordOfTheDay] = useState('');
     const [definition, setDefinition] = useState('');
 
@@ -22,12 +34,63 @@ function Home() {
 };
 
     return (
+        <Layout className="layout">
+        <Menu theme="dark" 
+    mode="horizontal"defaultSelectedKeys={['1']}
+    items={[
+      {
+        key: '1',
+        icon: <UserOutlined />,
+        label: 'Login',
+      },
+      {
+        key: '2',
+        icon: <VideoCameraOutlined />,
+        label: 'Signup',
+      }
+    ]}>
+    </Menu>
+        
+        <Header/>
+        
         <div className='home'>
-            <Header />
-            <button onClick={handleClick}>Generate Word of The Day</button>
+            <Button size="lg" type="primary" onClick={handleClick}>Generate Word of The Day</Button>
             <h1>{wordOfTheDay}:</h1>
             <p>{definition}</p>
         </div>
+        <Content
+          style={{
+            padding: '0 50px',
+          }}
+        ><Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>Test</Breadcrumb.Item>
+            <Breadcrumb.Item>Your</Breadcrumb.Item>
+            <Breadcrumb.Item>Knowledge</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-content"
+            style={{
+              background: colorBgContainer,
+            }}
+          >
+
+            Test your Knowledge with our amazing flashcard app!!
+            We have a collection of flashcard decks for every topic! 
+            Use the searchbar to find a deck for you.
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
+      </Layout>
     );
 }
 
