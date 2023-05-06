68a82034850e52ded664ebed8194a88f93e72e7b
Matthew M. Provost <provostmat@gmail.com>
2023-05-05 23:53:22 -0400

    reverted change to build file path

--- flashmaster-app/server/server.js
+++ flashmaster-app/server/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build/static')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('/', (req, res) => {
