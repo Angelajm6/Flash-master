71b6c6c1b4f88335d3ca523b7aee795e9fc1e562
Matthew M. Provost <provostmat@gmail.com>
2023-05-07 19:07:26 -0400

    trying to get anything to render on heroku

--- flashmaster-app/server/server.js
+++ flashmaster-app/server/server.js
@@ -17,10 +17,6 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-app.use('/assets', express.static(path.join(__dirname, '../client/public/assets')));
-
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
